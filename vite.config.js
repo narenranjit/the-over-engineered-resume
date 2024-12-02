dec5d8c6c858f5d29fca6dcc83d68ca87e6c35cf
Naren Ranjit <narendran.ranjit@coursehero.com>
2024-12-02 10:58:34 -0800

    allow adding ids to markdown

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { plugin as mdPlugin} from "vite-plugin-markdown";
+import markdownIt from 'markdown-it';
+import markdownItAttrs from 'markdown-it-attrs';
+
+const md = markdownIt();
+md.use(markdownItAttrs);
 
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [mdPlugin({
     mode: "html",
+    markdownIt: md
   })]
   // ...
 }
