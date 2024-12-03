82434eafc1ab49b7c569c211f339ecf881b7f889
Naren Ranjit <narendran.ranjit@coursehero.com>
2024-12-02 19:11:21 -0800

    delete redundant

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { plugin as mdPlugin, Mode } from "vite-plugin-markdown";
+import markdownIt from "markdown-it";
+import markdownItAttrs from "markdown-it-attrs";
 
+const md = markdownIt({
+  html: true,
+  typographer: true,
+});
+md.use(markdownItAttrs);
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    mdPlugin({
+      mode: [Mode.HTML],
+      markdownIt: md,
+    }),
+  ],
+});
